c4e666344cc60ad19db9ed3cfc21e6792384a264
mishtu <dyadyachernomor@gmail.com>
2023-04-04 18:48:56 +0500

    Implement the 'Edit' button

--- gui.py
+++ gui.py
@@ -5,20 +5,37 @@ import PySimpleGUI as psg
 label = psg.Text("Type in a to-do")
 input_box = psg.InputText(tooltip="Enter a to-do", key="todo")
 add_button = psg.Button("Add")
+list_box = psg.Listbox(values=functions.get_todos(), key="todos",
+                       enable_events=True, size=[45, 10])
+edit_button = psg.Button("Edit")
 
 window = psg.Window('To-Do App',
-                    layout=[[label], [input_box, add_button]],
+                    layout=[[label], [input_box, add_button], [list_box, edit_button]],
                     font=('Helvetica', 20))
 
 while True:
     event, values = window.read()
-
+    print(event)
+    print(values)
+    print(values['todos'])
     match event:
         case 'Add':
             todos = functions.get_todos()
             new_todo = values['todo'] + '\n'
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case 'Edit':
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo'] + '\n'
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
         case psg.WINDOW_CLOSED:
             break
 
