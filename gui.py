ef9f1d8ade1f1801910eb92d7857c56dd3097f78
mishtu <dyadyachernomor@gmail.com>
2023-04-05 15:57:41 +0500

    Cosmetic changes for buttons 'Add' and 'Complete', fix the bug when trying to add empty todo

--- gui.py
+++ gui.py
@@ -7,11 +7,13 @@ psg.theme("Dark")
 clock = psg.Text("", key="clock")
 label = psg.Text("Type in a to-do")
 input_box = psg.InputText(tooltip="Enter a to-do", key="todo")
-add_button = psg.Button("Add")
+add_button = psg.Button(key="Add", image_size=(50, 40),
+                        image_source="add.png", tooltip="Add Todo")
 list_box = psg.Listbox(values=functions.get_todos(), key="todos",
                        enable_events=True, size=[45, 10])
 edit_button = psg.Button("Edit")
-complete_button = psg.Button("Complete")
+complete_button = psg.Button(key="Complete", image_size=(62, 43),
+                             image_source="complete.png", tooltip="Complete")
 exit_button = psg.Button("Exit")
 
 window = psg.Window('To-Do App',
@@ -30,9 +32,12 @@ while True:
         case 'Add':
             todos = functions.get_todos()
             new_todo = values['todo'] + '\n'
-            todos.append(new_todo)
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
+            if new_todo == '\n':
+                continue
+            else:
+                todos.append(new_todo)
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
         case 'Edit':
             try:
                 todo_to_edit = values['todos'][0]
